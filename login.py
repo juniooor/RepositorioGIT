1da096986dba2a7b4f6f1c0f1cf644a751cfe0b7
Willams Junior <juniormodern@hotmail.com>
2022-04-11 14:49:15 -0300

    adcionado resgate de senha

--- login.py
+++ login.py
@@ -1,7 +1,9 @@
 email = str(input('EMAIL: '))
 senha = str(input('SENHA: '))
+invalid = str(input("Esqueceu a senha?: [S/N]"))
 
 if email == 'emailteste@hotmail.com' and senha == '1a2b3c45':
     print('LOGIN EFETUADO COM SUCESSO')
 else:
     print('EMAIL OU SENHA INVALIDOS')
+    
