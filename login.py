096d76cc5c23702fabf875b93e0ad41e507bf236
Willams Junior <juniormodern@hotmail.com>
2022-04-11 14:57:50 -0300

    adcionado idade

--- login.py
+++ login.py
@@ -1,5 +1,6 @@
 email = str(input('EMAIL: '))
 senha = str(input('SENHA: '))
+password = int(input('qual sua idade?: '))
 
 if email == 'emailteste@hotmail.com' and senha == '1a2b3c45':
     print('LOGIN EFETUADO COM SUCESSO')
