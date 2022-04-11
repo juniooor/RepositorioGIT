346d513c20674c407b8e9ef7a1900acfba454df1
Willams Junior <juniormodern@hotmail.com>
2022-04-11 14:38:15 -0300

    Criado sistema de login

---
+++ login.py
@@ -0,0 +1,7 @@
+email = str(input('EMAIL: '))
+senha = str(input('SENHA: '))
+
+if email == 'emailteste@hotmail.com' and senha == '1a2b3c45':
+    print('LOGIN EFETUADO COM SUCESSO')
+else:
+    print('EMAIL OU SENHA INVALIDOS')
